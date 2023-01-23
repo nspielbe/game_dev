41d0006ec26490c159b9859a8e087d4edf8a0bdc
nspielbe <nspielbe@gmail.com>
2023-01-22 23:53:51 -0500

    Finish fraction exercise

--- learn_cpp_programming_by_making_games_2/section_3/Fraction.cpp
+++ learn_cpp_programming_by_making_games_2/section_3/Fraction.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+namespace{
+
+/// @brief Use Euclid's Algorithm for recursively computing the GCD
+/// @param a 
+/// @param b 
+/// @return an int representing the GCD
+int greatestCommonDenominator(int a, int b){
+    if (b == 0){
+        return a;
+    }
+    else{
+        return greatestCommonDenominator(b, a % b);
+    }
+}
+
 class Fraction
 {
 public:
@@ -16,18 +31,31 @@ public:
     Fraction multiply(Fraction fraction);
     Fraction divide(Fraction fraction);
 
+
+private:
     // Getter functions for accessing private data members
     int getNumerator();
     int getDenominator();
 
     // Utility function for reducting a fraction to lowest common denominator
-    void reduce();
     int getCommonDenominator(Fraction fraction);
-    
-private:
+    void reduce();
+
     int numerator;
     int denominator;
 };
+
+void Fraction::reduce(){
+    int gcd = greatestCommonDenominator(numerator, denominator);
+    if (gcd > 0){
+        numerator = numerator / gcd;
+        denominator = denominator / gcd; 
+    }
+}
+
+int Fraction::getCommonDenominator(Fraction fraction){
+    return fraction.getDenominator() * denominator;
+}
 
 int Fraction::getDenominator(){
     return denominator;
@@ -48,22 +76,60 @@ void Fraction::display(){
     std::cout << "Numerator: " << numerator << " Denominator: " << denominator << std::endl;
 }
 
-
 Fraction Fraction::multiply(Fraction fraction){
-    return {fraction.getNumerator() * numerator, fraction.getDenominator() * denominator};
+    Fraction result = {fraction.getNumerator() * numerator, fraction.getDenominator() * denominator};
+    result.reduce();
+    return result;
 }
 
 Fraction Fraction::divide(Fraction fraction){
-    return {fraction.getNumerator() * denominator, fraction.getDenominator() * numerator};
+    Fraction result = {fraction.getNumerator() * denominator, fraction.getDenominator() * numerator};
+    result.reduce();
+    return result;
 }
 
+Fraction Fraction::add(Fraction fraction){
+    // get the common denominator
+    int commonDenom = getCommonDenominator(fraction);
+
+    int newNumerator = (commonDenom/fraction.getDenominator()) * fraction.getNumerator()
+                     + (commonDenom/denominator) * numerator;
+
+    Fraction result = {newNumerator, commonDenom};
+    result.reduce();
+    return result;   
+}
+
+Fraction Fraction::subtract(Fraction fraction){
+    // get the common denominator
+    int commonDenom = getCommonDenominator(fraction);
+
+    int newNumerator = (commonDenom/denominator) * numerator
+                        -(commonDenom/fraction.getDenominator()) * fraction.getNumerator();
+
+    Fraction result = {newNumerator, commonDenom};
+    result.reduce();
+    return result;  
+}
+
+} // end anon namespace
 
 int main(){
     Fraction defaultFrac = Fraction();
     defaultFrac.display();
     
-    Fraction myFrac = {1,2};
-    myFrac.display();
+    Fraction myFrac{1,2};
+    Fraction myFrac2{2,5};
+
+    Fraction result = myFrac2.add(myFrac);
+    Fraction result2 = myFrac2.subtract(myFrac);
+    Fraction result3 = myFrac2.multiply(myFrac);
+    Fraction result4 = myFrac2.divide(myFrac);
+
+    result.display();
+    result2.display();
+    result3.display();
+    result4.display();
 
     return 0;
 }
